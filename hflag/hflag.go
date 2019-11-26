8616b181edd33acb8b134d5e3d83e7ceba795879
hatlonely <hatlonely@gmail.com>
2019-11-27 02:07:19 +0800

    update

--- hflag/hflag.go
+++ hflag/hflag.go
@@ -136,7 +136,7 @@ func (f *FlagSet) Parse(args []string) error {
 		if strings.HasPrefix(arg, "--") {
 			option = arg[2:]
 		}
-		if strings.Contains(option, "=") {
+		if strings.Contains(option, "=") { // 选项中含有等号，按照等号分割成 name val
 			idx := strings.Index(option, "=")
 			name := option[0:idx]
 			val := option[idx+1:]
@@ -153,7 +153,7 @@ func (f *FlagSet) Parse(args []string) error {
 			if flag == nil {
 				return fmt.Errorf("unknow flag [%v]", name)
 			}
-			if flag.Type != "bool" { // 参数不是 bool，后面必有一个值
+			if flag.Type != "bool" { // 选项不是 bool，后面必有一个值
 				if i+1 >= len(args) {
 					return fmt.Errorf("miss argument for nonboolean option [%v]", name)
 				}
@@ -162,7 +162,7 @@ func (f *FlagSet) Parse(args []string) error {
 					return fmt.Errorf("set failed. name: [%v], val: [%v], type: [%v]", name, val, flag.Type)
 				}
 				i++
-			} else { // 参数为 bool 类型，如果后面的值为 true 或者 false 则设为后面值，否则设置为 true
+			} else { // 选项为 bool 类型，如果后面的值为合法的 bool 值，否则设置为 true
 				val := "true"
 				if i+1 < len(args) && isBoolValue(args[i+1]) {
 					val = args[i+1]
@@ -172,7 +172,7 @@ func (f *FlagSet) Parse(args []string) error {
 					return fmt.Errorf("set failed. name: [%v], val: [%v], type: [%v]", name, val, flag.Type)
 				}
 			}
-		} else if f.allBoolFlag(option) { // -kval 全是 bool 选项，-kval 和 -k -v -f -l 等效
+		} else if f.allBoolFlag(option) { // -aux 全是 bool 选项，-aux 和 -a -u -x 等效
 			for i := 0; i < len(option); i++ {
 				name := option[i : i+1]
 				flag := f.Lookup(name)
@@ -180,7 +180,7 @@ func (f *FlagSet) Parse(args []string) error {
 					return fmt.Errorf("set failed. name: [%v], val: [%v], type: [%v]", name, "true", flag.Type)
 				}
 			}
-		} else {
+		} else { // -p123456 和 -p 123456 等效
 			name := option[0:1]
 			val := option[1:]
 			flag := f.Lookup(name)
