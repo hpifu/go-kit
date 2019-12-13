c7a811bd7333fbf189f8e6ec3b4d3a19b5d6b8db
hatlonely <hatlonely@gmail.com>
2019-12-14 03:45:20 +0800

    update

--- henv/henv.go
+++ henv/henv.go
@@ -1,8 +1,13 @@
 package henv
 
 import (
+	"fmt"
+	"github.com/hpifu/go-kit/hstring"
+	"net"
 	"os"
+	"reflect"
 	"strings"
+	"time"
 )
 
 func NewHEnv(prefix string) *HEnv {
@@ -33,4 +38,258 @@ func NewHEnv(prefix string) *HEnv {
 type HEnv struct {
 	prefix string
 	kvs    map[string]string
+}
+
+func (h HEnv) Unmarshal(v interface{}) error {
+	return mapToStruct(h.kvs, v, "")
+}
+
+func mapToStruct(kvs map[string]string, v interface{}, prefix string) error {
+	if kvs == nil {
+		return nil
+	}
+	if reflect.ValueOf(v).Kind() != reflect.Ptr {
+		return fmt.Errorf("invalid value type [%v]", reflect.TypeOf(v))
+	}
+
+	rv := reflect.ValueOf(v).Elem()
+	rt := reflect.TypeOf(v).Elem()
+	switch rt.Kind() {
+	case reflect.Struct:
+		for i := 0; i < rv.NumField(); i++ {
+			field := rv.Field(i)
+			key := rt.Field(i).Tag.Get("henv")
+			if key == "-" {
+				continue
+			}
+			if key == "" {
+				key = hstring.SnakeNameAllCaps(rt.Field(i).Name)
+			}
+			if prefix != "" {
+				key = prefix + "_" + key
+			}
+			if rt.Field(i).Type.Kind() == reflect.Ptr {
+				if field.IsNil() {
+					nv := reflect.New(field.Type().Elem())
+					field.Set(nv)
+				}
+				if err := mapToStruct(kvs, field.Interface(), key); err != nil {
+					return err
+				}
+			} else {
+				if err := mapToStruct(kvs, field.Addr().Interface(), key); err != nil {
+					return err
+				}
+			}
+		}
+	default:
+		val, ok := kvs[prefix]
+		if !ok || val == "" {
+			return nil
+		}
+		switch rv.Interface().(type) {
+		case string:
+			rv.Set(reflect.ValueOf(val))
+		case bool:
+			v, err := hstring.ToBool(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case int:
+			v, err := hstring.ToInt(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case uint:
+			v, err := hstring.ToUint(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case int64:
+			v, err := hstring.ToInt64(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case int32:
+			v, err := hstring.ToInt32(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case int16:
+			v, err := hstring.ToInt16(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case int8:
+			v, err := hstring.ToInt8(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case uint64:
+			v, err := hstring.ToUint64(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case uint32:
+			v, err := hstring.ToUint32(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case uint16:
+			v, err := hstring.ToUint16(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case uint8:
+			v, err := hstring.ToUint8(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case float64:
+			v, err := hstring.ToFloat64(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case float32:
+			v, err := hstring.ToFloat32(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case time.Duration:
+			v, err := hstring.ToDuration(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case time.Time:
+			v, err := hstring.ToTime(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case net.IP:
+			v, err := hstring.ToIP(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []string:
+			v, err := hstring.ToStringSlice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []bool:
+			v, err := hstring.ToBoolSlice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []int:
+			v, err := hstring.ToIntSlice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []uint:
+			v, err := hstring.ToUintSlice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []int64:
+			v, err := hstring.ToInt64Slice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []int32:
+			v, err := hstring.ToInt32Slice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []int16:
+			v, err := hstring.ToInt16Slice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []int8:
+			v, err := hstring.ToInt8Slice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []uint64:
+			v, err := hstring.ToUint64Slice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []uint32:
+			v, err := hstring.ToUint32Slice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []uint16:
+			v, err := hstring.ToUint16Slice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []uint8:
+			v, err := hstring.ToUint8Slice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []float64:
+			v, err := hstring.ToFloat64Slice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []float32:
+			v, err := hstring.ToFloat32Slice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []time.Duration:
+			v, err := hstring.ToDurationSlice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []time.Time:
+			v, err := hstring.ToTimeSlice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case []net.IP:
+			v, err := hstring.ToIPSlice(val)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		}
+	}
+
+	return nil
 }
--- henv/henv_test.go
+++ henv/henv_test.go
@@ -2,12 +2,13 @@ package henv
 
 import (
 	. "github.com/smartystreets/goconvey/convey"
+	"net"
 	"os"
 	"testing"
 )
 
 func TestHEnv(t *testing.T) {
-	Convey("test env", t, func() {
+	Convey("test env getter", t, func() {
 		_ = os.Setenv("TEST_INT", "123")
 		_ = os.Setenv("TEST_INT_SLICE", "1,2,3")
 		he := NewHEnv("TEST")
@@ -27,5 +28,31 @@ func TestHEnv(t *testing.T) {
 		v, err := he.GetString("NOT_EXIST_KEY")
 		So(err, ShouldEqual, NoSuchKeyErr)
 		So(v, ShouldEqual, "")
+	})
+
+	Convey("test env unmarshal", t, func() {
+		type MySubStruct struct {
+			Vi        []int
+			IPAddress net.IP
+		}
+
+		type MyStruct struct {
+			I   *int `henv:"INT"`
+			S   string
+			Sub MySubStruct
+		}
+
+		_ = os.Setenv("TEST_INT", "123")
+		_ = os.Setenv("TEST_S", "hatlonely")
+		_ = os.Setenv("TEST_SUB_VI", "1,2,3")
+		_ = os.Setenv("TEST_SUB_IP_ADDRESS", "114.243.208.244")
+
+		he := NewHEnv("TEST")
+		ms := &MyStruct{}
+		So(he.Unmarshal(ms), ShouldBeNil)
+		So(*ms.I, ShouldEqual, 123)
+		So(ms.S, ShouldEqual, "hatlonely")
+		So(ms.Sub.Vi, ShouldResemble, []int{1, 2, 3})
+		So(ms.Sub.IPAddress.String(), ShouldEqual, "114.243.208.244")
 	})
 }
