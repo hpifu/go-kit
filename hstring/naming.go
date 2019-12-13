483667fb216d10a95e7bf510dbfcbd6da9082988
hatlonely <hatlonely@gmail.com>
2019-12-13 23:38:36 +0800

    update

---
+++ hstring/naming.go
@@ -0,0 +1,207 @@
+package hstring
+
+import (
+	"bytes"
+)
+
+func IsUpper(ch uint8) bool {
+	return ch >= 'A' && ch <= 'Z'
+}
+
+func IsLower(ch uint8) bool {
+	return ch >= 'a' && ch <= 'z'
+}
+
+func IsDigit(ch uint8) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func ToLower(ch uint8) uint8 {
+	if IsUpper(ch) {
+		return ch + 32
+	}
+	return ch
+}
+
+func ToUpper(ch uint8) uint8 {
+	if IsLower(ch) {
+		return ch - 32
+	}
+	return ch
+}
+
+func CamelName(str string) string {
+	var buf bytes.Buffer
+	firstWord := true
+	for i := 0; i < len(str); {
+		for str[i] == '_' || str[i] == '-' {
+			i++
+		}
+		j := i
+		for j < len(str) && IsUpper(str[j]) {
+			j++
+		}
+		if j-i < 2 { // 只有一个大写字母
+			for ; j < len(str); j++ {
+				if str[j] == '_' || str[j] == '-' || IsUpper(str[j]) {
+					break
+				}
+			}
+		} else {
+			if j < len(str) && str[j] != '-' && str[j] != '_' {
+				j--
+			}
+		}
+		if j-i >= 2 && IsUpper(str[i]) && IsUpper(str[i+1]) { // HELLO
+			if firstWord {
+				firstWord = false
+				for k := i; k < j; k++ {
+					buf.WriteByte(ToLower(str[k]))
+				}
+			} else {
+				for k := i; k < j; k++ {
+					buf.WriteByte(str[k])
+				}
+			}
+		} else if IsUpper(str[i]) { // Hello
+			if firstWord {
+				buf.WriteByte(ToLower(str[i]))
+				firstWord = false
+			} else {
+				buf.WriteByte(str[i])
+			}
+			for k := i + 1; k < j; k++ {
+				buf.WriteByte(str[k])
+			}
+		} else { // hello
+			if firstWord {
+				buf.WriteByte(str[i])
+				firstWord = false
+			} else {
+				buf.WriteByte(ToUpper(str[i]))
+			}
+			for k := i + 1; k < j; k++ {
+				buf.WriteByte(str[k])
+			}
+		}
+		i = j
+	}
+
+	return buf.String()
+}
+
+func PascalName(str string) string {
+	var buf bytes.Buffer
+	firstWord := true
+	for i := 0; i < len(str); {
+		for str[i] == '_' || str[i] == '-' {
+			i++
+		}
+		j := i
+		for j < len(str) && IsUpper(str[j]) {
+			j++
+		}
+		if j-i < 2 { // 只有一个大写字母
+			for ; j < len(str); j++ {
+				if str[j] == '_' || str[j] == '-' || IsUpper(str[j]) {
+					break
+				}
+			}
+		} else {
+			if j < len(str) && str[j] != '-' && str[j] != '_' {
+				j--
+			}
+		}
+		if j-i >= 2 && IsUpper(str[i]) && IsUpper(str[i+1]) { // HELLO
+			if firstWord {
+				firstWord = false
+			}
+			for k := i; k < j; k++ {
+				buf.WriteByte(str[k])
+			}
+		} else if IsUpper(str[i]) { // Hello
+			if firstWord {
+				firstWord = false
+			}
+			for k := i; k < j; k++ {
+				buf.WriteByte(str[k])
+			}
+		} else { // hello
+			if firstWord {
+				firstWord = false
+			}
+			buf.WriteByte(ToUpper(str[i]))
+			for k := i + 1; k < j; k++ {
+				buf.WriteByte(str[k])
+			}
+		}
+		i = j
+	}
+
+	return buf.String()
+}
+
+func SnakeName(str string) string {
+	return snakeName(str, '_')
+}
+
+func KebabName(str string) string {
+	return snakeName(str, '-')
+}
+
+func snakeName(str string, separator uint8) string {
+	var buf bytes.Buffer
+	firstWord := true
+	for i := 0; i < len(str); {
+		for str[i] == '_' || str[i] == '-' {
+			i++
+		}
+		j := i
+		for j < len(str) && IsUpper(str[j]) {
+			j++
+		}
+		if j-i < 2 { // 只有一个大写字母
+			for ; j < len(str); j++ {
+				if str[j] == '_' || str[j] == '-' || IsUpper(str[j]) {
+					break
+				}
+			}
+		} else {
+			if j < len(str) && str[j] != '-' && str[j] != '_' {
+				j--
+			}
+		}
+		if j-i >= 2 && IsUpper(str[i]) && IsUpper(str[i+1]) { // HELLO
+			if firstWord {
+				firstWord = false
+			} else {
+				buf.WriteByte(separator)
+			}
+			for k := i; k < j; k++ {
+				buf.WriteByte(ToLower(str[k]))
+			}
+		} else if IsUpper(str[i]) { // Hello
+			if firstWord {
+				firstWord = false
+			} else {
+				buf.WriteByte(separator)
+			}
+			buf.WriteByte(ToLower(str[i]))
+			for k := i + 1; k < j; k++ {
+				buf.WriteByte(str[k])
+			}
+		} else { // hello
+			if firstWord {
+				firstWord = false
+			} else {
+				buf.WriteByte(separator)
+			}
+			for k := i; k < j; k++ {
+				buf.WriteByte(str[k])
+			}
+		}
+		i = j
+	}
+
+	return buf.String()
+}
---
+++ hstring/naming_test.go
@@ -0,0 +1,64 @@
+package hstring
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestNaming(t *testing.T) {
+	Convey("test  naming", t, func() {
+		Convey("camel name", func() {
+			So(CamelName("hello_world"), ShouldEqual, "helloWorld")
+			So(CamelName("HelloWorld"), ShouldEqual, "helloWorld")
+			So(CamelName("Hello_World"), ShouldEqual, "helloWorld")
+			So(CamelName("hello_WorldWorld"), ShouldEqual, "helloWorldWorld")
+			So(CamelName("IPAddress"), ShouldEqual, "ipAddress")
+			So(CamelName("HELLOWorld"), ShouldEqual, "helloWorld")
+			So(CamelName("helloWorld"), ShouldEqual, "helloWorld")
+			So(CamelName("MyIPAddress"), ShouldEqual, "myIPAddress")
+			So(CamelName("IP-Address"), ShouldEqual, "ipAddress")
+			So(CamelName("IP-AddressAPP"), ShouldEqual, "ipAddressAPP")
+
+		})
+
+		Convey("pascal name", func() {
+			So(PascalName("hello_world"), ShouldEqual, "HelloWorld")
+			So(PascalName("HelloWorld"), ShouldEqual, "HelloWorld")
+			So(PascalName("Hello_World"), ShouldEqual, "HelloWorld")
+			So(PascalName("hello_WorldWorld"), ShouldEqual, "HelloWorldWorld")
+			So(PascalName("IPAddress"), ShouldEqual, "IPAddress")
+			So(PascalName("HELLOWorld"), ShouldEqual, "HELLOWorld")
+			So(PascalName("helloWorld"), ShouldEqual, "HelloWorld")
+			So(PascalName("MyIPAddress"), ShouldEqual, "MyIPAddress")
+			So(PascalName("IP-Address"), ShouldEqual, "IPAddress")
+			So(PascalName("IP-AddressAPP"), ShouldEqual, "IPAddressAPP")
+
+		})
+
+		Convey("snake name", func() {
+			So(SnakeName("hello_world"), ShouldEqual, "hello_world")
+			So(SnakeName("HelloWorld"), ShouldEqual, "hello_world")
+			So(SnakeName("Hello_World"), ShouldEqual, "hello_world")
+			So(SnakeName("hello_WorldWorld"), ShouldEqual, "hello_world_world")
+			So(SnakeName("IPAddress"), ShouldEqual, "ip_address")
+			So(SnakeName("HELLOWorld"), ShouldEqual, "hello_world")
+			So(SnakeName("helloWorld"), ShouldEqual, "hello_world")
+			So(SnakeName("MyIPAddress"), ShouldEqual, "my_ip_address")
+			So(SnakeName("IP-Address"), ShouldEqual, "ip_address")
+			So(SnakeName("IP-AddressAPP"), ShouldEqual, "ip_address_app")
+		})
+
+		Convey("kebab name", func() {
+			So(KebabName("hello_world"), ShouldEqual, "hello-world")
+			So(KebabName("HelloWorld"), ShouldEqual, "hello-world")
+			So(KebabName("Hello_World"), ShouldEqual, "hello-world")
+			So(KebabName("hello_WorldWorld"), ShouldEqual, "hello-world-world")
+			So(KebabName("IPAddress"), ShouldEqual, "ip-address")
+			So(KebabName("HELLOWorld"), ShouldEqual, "hello-world")
+			So(KebabName("helloWorld"), ShouldEqual, "hello-world")
+			So(KebabName("MyIPAddress"), ShouldEqual, "my-ip-address")
+			So(KebabName("IP-Address"), ShouldEqual, "ip-address")
+			So(KebabName("IP-AddressAPP"), ShouldEqual, "ip-address-app")
+		})
+	})
+}
