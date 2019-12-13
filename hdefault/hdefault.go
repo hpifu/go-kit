4af0664efce278cd0e494a5c73c2a4cb90a7d2d4
hatlonely <hatlonely@gmail.com>
2019-12-13 17:10:31 +0800

    update

---
+++ hdefault/hdefault.go
@@ -0,0 +1,45 @@
+package hdefault
+
+import (
+	"fmt"
+	"github.com/hpifu/go-kit/hstring"
+	"reflect"
+)
+
+func SetDefault(v interface{}) error {
+	if reflect.ValueOf(v).Kind() != reflect.Ptr {
+		return fmt.Errorf("invalid value type %v", reflect.TypeOf(v))
+	}
+
+	if v == nil {
+		return fmt.Errorf("params should not be nil")
+	}
+
+	rv := reflect.ValueOf(v).Elem()
+	rt := reflect.TypeOf(v).Elem()
+
+	for i := 0; i < rv.NumField(); i++ {
+		field := rv.Field(i)
+		value := rt.Field(i).Tag.Get("hdef")
+		if field.Kind() == reflect.Ptr {
+			if field.IsNil() {
+				nv := reflect.New(rt.Field(i).Type.Elem())
+				field.Set(nv)
+			}
+			if err := SetDefault(field.Interface()); err != nil {
+				return err
+			}
+		} else if field.Kind() == reflect.Struct {
+			if err := SetDefault(field.Addr().Interface()); err != nil {
+				return err
+			}
+		} else {
+			err := hstring.SetValue(field, value)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
---
+++ hdefault/hdefault_test.go
@@ -0,0 +1,32 @@
+package hdefault
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestSetDefault(t *testing.T) {
+	Convey("test set", t, func() {
+		type MySubStruct struct {
+			Int   int     `hdef:"456"`
+			Float float32 `hdef:"123.456"`
+		}
+
+		type MyStruct struct {
+			Int      int    `hdef:"123"`
+			Str      string `hdef:"hello"`
+			IntSlice []int  `hdef:"1,2,3"`
+			Sub1     MySubStruct
+			Sub2     *MySubStruct
+		}
+		ms := &MyStruct{}
+		So(SetDefault(ms), ShouldBeNil)
+		So(ms.Int, ShouldEqual, 123)
+		So(ms.Str, ShouldEqual, "hello")
+		So(ms.IntSlice, ShouldResemble, []int{1, 2, 3})
+		So(ms.Sub1.Int, ShouldEqual, 456)
+		So(ms.Sub1.Float, ShouldAlmostEqual, float32(123.456))
+		So(ms.Sub2.Int, ShouldEqual, 456)
+		So(ms.Sub2.Float, ShouldEqual, float32(123.456))
+	})
+}
