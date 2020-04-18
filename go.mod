9c6c4769a61518df287709267143aec544d5f01a
hatlonely <hatlonely@gmail.com>
2020-04-18 23:22:28 +0800

    update

--- go.mod
+++ go.mod
@@ -2,26 +2,6 @@ module github.com/hpifu/go-kit
 
 go 1.14
 
-replace (
-	cloud.google.com/go => github.com/googleapis/google-cloud-go v0.0.0-20190603211518-c8433c9aaceb
-	golang.org/x/crypto => github.com/golang/crypto v0.0.0-20190605123033-f99c8df09eb5
-	golang.org/x/exp => github.com/golang/exp v0.0.0-20190510132918-efd6b22b2522
-	golang.org/x/image => github.com/golang/image v0.0.0-20190523035834-f03afa92d3ff
-	golang.org/x/lint => github.com/golang/lint v0.0.0-20190409202823-959b441ac422
-	golang.org/x/mobile => github.com/golang/mobile v0.0.0-20190607214518-6fa95d984e88
-	golang.org/x/net => github.com/golang/net v0.0.0-20190606173856-1492cefac77f
-	golang.org/x/oauth2 => github.com/golang/oauth2 v0.0.0-20190604053449-0f29369cfe45
-	golang.org/x/sync => github.com/golang/sync v0.0.0-20190423024810-112230192c58
-	golang.org/x/sys => github.com/golang/sys v0.0.0-20190602015325-4c4f7f33c9ed
-	golang.org/x/text => github.com/golang/text v0.3.2
-	golang.org/x/time => github.com/golang/time v0.0.0-20190308202827-9d24e82272b4
-	golang.org/x/tools => github.com/golang/tools v0.0.0-20190608022120-eacb66d2a7c3
-	google.golang.org/api => github.com/googleapis/google-api-go-client v0.6.0
-	google.golang.org/appengine => github.com/golang/appengine v1.6.1
-	google.golang.org/genproto => github.com/google/go-genproto v0.0.0-20190605220351-eb0b1bdb6ae6
-	google.golang.org/grpc => github.com/grpc/grpc-go v1.21.1
-)
-
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
@@ -35,7 +15,7 @@ require (
 	github.com/sirupsen/logrus v1.4.2
 	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337
 	github.com/spaolacci/murmur3 v0.0.0-20180118202830-f09979ecbc72
-	github.com/spf13/cast v1.3.0
+	github.com/spf13/cast v1.3.1
 	github.com/spf13/viper v1.4.0
 	github.com/tebeka/strftime v0.1.3 // indirect
 	github.com/yosuke-furukawa/json5 v0.1.1
---
+++ href/href.go
@@ -0,0 +1,213 @@
+package href
+
+import (
+	"fmt"
+	"net"
+	"reflect"
+	"time"
+
+	"github.com/spf13/cast"
+
+	"github.com/hpifu/go-kit/hstr"
+)
+
+func InterfaceToStruct(d interface{}, v interface{}) error {
+	if d == nil {
+		return nil
+	}
+	if reflect.ValueOf(v).Kind() != reflect.Ptr {
+		return fmt.Errorf("invalid value type, expect a pointer, got %v", reflect.TypeOf(v))
+	}
+
+	rv := reflect.ValueOf(v).Elem()
+	rt := reflect.TypeOf(v).Elem()
+	switch rt.Kind() {
+	case reflect.Struct:
+		if dv, ok := d.(map[string]interface{}); ok {
+			for i := 0; i < rv.NumField(); i++ {
+				field := rv.Field(i)
+				key := rt.Field(i).Tag.Get("hconf")
+				if key == "-" {
+					continue
+				}
+				if key == "" {
+					key = hstr.CamelName(rt.Field(i).Name)
+				}
+				value := dv[key]
+				if rt.Field(i).Type.Kind() == reflect.Ptr {
+					if field.IsNil() {
+						nv := reflect.New(field.Type().Elem())
+						field.Set(nv)
+					}
+					if err := interfaceToStruct(value, field.Interface()); err != nil {
+						return err
+					}
+				} else {
+					if err := interfaceToStruct(value, field.Addr().Interface()); err != nil {
+						return err
+					}
+				}
+			}
+		} else if dv, ok := d.(map[interface{}]interface{}); ok {
+			for i := 0; i < rv.NumField(); i++ {
+				field := rv.Field(i)
+				key := rt.Field(i).Tag.Get("hconf")
+				if key == "-" {
+					continue
+				}
+				if key == "" {
+					key = hstr.CamelName(rt.Field(i).Name)
+				}
+				value, ok := dv[key].(string)
+				if !ok {
+					return fmt.Errorf("convert data to string failed. which is %v", reflect.TypeOf(d))
+				}
+				if rt.Field(i).Type.Kind() == reflect.Ptr {
+					if field.IsNil() {
+						nv := reflect.New(field.Type().Elem())
+						field.Set(nv)
+					}
+					if err := interfaceToStruct(value, field.Interface()); err != nil {
+						return err
+					}
+				} else {
+					if err := interfaceToStruct(value, field.Addr().Interface()); err != nil {
+						return err
+					}
+				}
+			}
+		} else {
+			return fmt.Errorf("convert data to map[string]interface{} failed. which is %v", reflect.TypeOf(d))
+		}
+	case reflect.Slice:
+		dv, ok := d.([]interface{})
+		if !ok {
+			return fmt.Errorf("convert data to []interface{} failed. which is %v", reflect.TypeOf(d))
+		}
+		rv.Set(reflect.MakeSlice(rt, 0, rv.Cap()))
+		for _, di := range dv {
+			nv := reflect.New(rt.Elem())
+			err := interfaceToStruct(di, nv.Interface())
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.Append(rv, nv.Elem()))
+		}
+	default:
+		switch rv.Interface().(type) {
+		case string:
+			v, err := cast.ToStringE(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case bool:
+			v, err := cast.ToBoolE(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case int:
+			v, err := cast.ToIntE(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case uint:
+			v, err := cast.ToUintE(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case int64:
+			v, err := cast.ToInt64E(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case int32:
+			v, err := cast.ToInt32E(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case int16:
+			v, err := cast.ToInt16E(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case int8:
+			v, err := cast.ToInt8E(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case uint64:
+			v, err := cast.ToUint64E(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case uint32:
+			v, err := cast.ToUint32E(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case uint16:
+			v, err := cast.ToUint16E(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case uint8:
+			v, err := cast.ToUint8E(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case float64:
+			v, err := cast.ToFloat64E(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case float32:
+			v, err := cast.ToFloat32E(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case time.Duration:
+			v, err := cast.ToDurationE(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case time.Time:
+			v, err := cast.ToTimeE(d)
+			if err != nil {
+				return err
+			}
+			rv.Set(reflect.ValueOf(v))
+		case net.IP:
+			switch v.(type) {
+			case string:
+				v, err := hstr.ToIP(v.(string))
+				if err != nil {
+					return err
+				}
+				rv.Set(reflect.ValueOf(v))
+			case net.IP:
+				rv.Set(reflect.ValueOf(v.(net.IP)))
+			default:
+				return fmt.Errorf("convert type [%v] to ip failed", reflect.TypeOf(v))
+			}
+		default:
+			return fmt.Errorf("unsupport type %v", rt)
+		}
+	}
+
+	return nil
+}
